590dd63bc5470a9de722265ac6b9fe2710624083
sagar14vijay <sgvijay1402@gmail.com>
2021-09-29 15:41:06 +0530

    Create merge_two_soted_list.py

---
+++ Problems/merge_two_soted_list.py
@@ -0,0 +1,14 @@
+# Given two sorted list. We need to merge these two list such that the initial numbers (after complete sorting)
+# are in the first list and the remaining numbers are in the second list. Extra space allowed in O(1).
+#
+# Example:
+#
+# Input: ar1 = [10]
+#        ar2 = [2, 3]
+# Output: ar1 = [2]
+#         ar2 = [3, 10]
+#
+# Input: ar1 = [1, 5, 9, 10, 15, 20]
+#        ar2 = [2, 3, 8, 13]
+# Output: ar1 = [1, 2, 3, 5, 8, 9]
+#         ar2 = [10, 13, 15, 20]
